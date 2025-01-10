5c64d5c7bf3daffe3b048a826ad16b02478d7fd3
Admin <1119321.ab@mail.ru>
2025-01-10 16:05:11 +0100

    lesson02

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 
 import './App.css';
 import Homework01 from './homeworks/homework01/Homework01';
+import Lesson01 from './lessons/lesson01/Lesson01';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+      <Lesson01/>
     <Homework01/>  
     </>
   );
